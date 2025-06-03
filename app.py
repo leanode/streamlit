6241fcddc53f35da69dfb48d8696dd7608de664c
Angela <leanode@gmail.com>
2025-06-03 16:55:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ import streamlit as st
 import pandas as pd
 import openai
 
-import os
-st.write("📁 Current working directory:", os.getcwd())
-st.write("📂 Files in this directory:", os.listdir())
-
 # 🔐 Load OpenAI API key
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
